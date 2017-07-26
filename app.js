a002f3965372824fb1f2194921998e8304ae67bc
Jeff Irwin <jeff.irwin@live.com>
2017-07-26 16:03:09 -0500

    setup main.css and mostly finished index.mustache

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const app = express();
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
-app.set('views', __dirname + 'views');
+app.set('views', __dirname + '/views');
 
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.render('index', data);
+});
 
 app.listen(3000, function() {
   console.log('Listening at port 3000');
